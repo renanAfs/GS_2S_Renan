08d6515db885866869e4a6058e1c6890ccd029ab
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-13 20:50:43 -0300

    Trigger Pipeline completa

--- terraform/aws/modules/compute/scripts/userdata.sh
+++ terraform/aws/modules/compute/scripts/userdata.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-    
-echo "Update with latest packages"
+
+echo "Update/Install required OS packages"
 yum update -y
-    
-echo "Install Apache"
-yum install -y httpd git
-    
-echo "Enable Apache service to start after reboot"
-sudo systemctl enable httpd
-    
-echo "Install application"
+yum install -y httpd wget php-fpm php-mysqli php-json php php-devel telnet tree git
+
+echo "Deploy PHP info app"
 cd /tmp
-git clone https://github.com/kledsonhugo/app-staticsite
-cp /tmp/app-staticsite/*.html /var/www/html/
-    
-echo "Start Apache service"
+git clone https://github.com/kledsonhugo/app-dynamicsite
+cp /tmp/app-dynamicsite/phpinfo.php /var/www/html/index.php
+
+echo "Config Apache WebServer"
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+
+echo "Start Apache WebServer"
+systemctl enable httpd
 service httpd restart
--- terraform/azure/modules/network/network.tf
+++ terraform/azure/modules/network/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
     name     = "rg-gsrenan"
-    location = "canadacentral"
+    location = "brazilsouth"
 }
 
 resource "azurerm_virtual_network" "vneta" {
