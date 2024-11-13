d4c5cde65040d4cb4bb4b2ba7d2232abf70767ab
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-13 20:33:21 -0300

    Trigger Pipeline completa

--- terraform/aws/mainAWS.tf
+++ terraform/aws/mainAWS.tf
@@ -4,7 +4,7 @@ module "network" {
 
 module "compute" {
  source = "./modules/compute"
- ec2_ami = "0f409bae3775dc8e5"
+ ec2_ami = "ami-0f409bae3775dc8e5"
  vpca_id = module.network.vpca_id
  sn_vpca_pub1a = module.network.sn_vpca_pub1a
  sn_vpca_pub1c = module.network.sn_vpca_pub1c
