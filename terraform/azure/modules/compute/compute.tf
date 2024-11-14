3871c469b07bdec8c4cca504b70ddb9c62ac4192
Laboratório FIAP <labsfiap@fiap.com.br>
2024-11-13 21:33:05 -0300

    Trigger Pipeline completa

--- terraform/azure/modules/compute/compute.tf
+++ terraform/azure/modules/compute/compute.tf
@@ -40,15 +40,15 @@ resource "azurerm_public_ip" "vm01_pip_public" {
     location            = var.rglocation
   resource_group_name = var.rgname
     allocation_method   = "Static"
-    domain_name_label   = "vm01-pip-public1a"
+    domain_name_label   = "vm01-pip-public"
 }
 
-resource "azurerm_network_interface" "vm01_nic_public1a" {
+resource "azurerm_network_interface" "vm01_nic_public" {
     name                = "vm01-nic-public"
     location            = var.rglocation
   resource_group_name = var.rgname
     ip_configuration {
-        name                          = "vm01-ipconfig-public1a"
+        name                          = "vm01-ipconfig-public"
         subnet_id                     = var.snvnetapub1a
         private_ip_address_allocation = "Dynamic"
         public_ip_address_id          = azurerm_public_ip.vm01_pip_public.id
@@ -59,8 +59,8 @@ resource "azurerm_virtual_machine" "vm01_public" {
     name                          = "vm01-public"
     location            = var.rglocation
   resource_group_name = var.rgname
-    network_interface_ids         = [azurerm_network_interface.vm01_nic_public1a.id]
-    vm_size                       = "Standard_E2s_v3"
+    network_interface_ids         = [azurerm_network_interface.vm01_nic_public.id]
+    vm_size                       = "Standard_D2s_v3"
     delete_os_disk_on_termination = true
     storage_image_reference {
         publisher = "Canonical"
@@ -78,20 +78,27 @@ resource "azurerm_virtual_machine" "vm01_public" {
         admin_username = "azureuser"
         admin_password = "Password1234!" 
         custom_data    = <<-EOF
-         #!/bin/bash
-            sudo apt-get update
-            sudo apt-get install -y python3 python3-pip
-            pip3 install flask
-             cat << 'EOF2' > /home/ubuntu/app.py
-            from flask import Flask
-             app = Flask(__name__)
-             @app.route('/')
-             def home():
-            return "Hello, Azure from Flask!"
-             if __name__ == '__main__':
-          app.run(host='0.0.0.0', port=5000)
-          EOF2
-          nohup python3 /home/ubuntu/app.py &
+        #!/bin/bash
+
+echo "Update/Install required OS packages"
+yum update -y
+yum install -y httpd wget php-fpm php-mysqli php-json php php-devel telnet tree git
+
+echo "Deploy PHP info app"
+cd /tmp
+git clone https://github.com/kledsonhugo/app-dynamicsite
+cp /tmp/app-dynamicsite/phpinfo.php /var/www/html/index.php
+
+echo "Config Apache WebServer"
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+
+echo "Start Apache WebServer"
+systemctl enable httpd
+service httpd restart
         EOF 
     }
     os_profile_linux_config {
@@ -126,7 +133,7 @@ resource "azurerm_virtual_machine" "vm02_public" {
     location            = var.rglocation
   resource_group_name = var.rgname
     network_interface_ids         = [azurerm_network_interface.vm02_nic_public.id]
-    vm_size                       = "Standard_E2s_v3"
+    vm_size                       = "Standard_D2s_v3"
     delete_os_disk_on_termination = true
     storage_image_reference {
         publisher = "Canonical"
@@ -142,57 +149,64 @@ resource "azurerm_virtual_machine" "vm02_public" {
     os_profile {
         computer_name  = "vm02-public"
         admin_username = "azureuser"
-        admin_password = "Password1234!"
+        admin_password = "Password1234!" 
         custom_data    = <<-EOF
-         #!/bin/bash
-            sudo apt-get update
-            sudo apt-get install -y python3 python3-pip
-            pip3 install flask
-             cat << 'EOF2' > /home/ubuntu/app.py
-            from flask import Flask
-             app = Flask(__name__)
-             @app.route('/')
-             def home():
-            return "Hello, Azure from Flask!"
-             if __name__ == '__main__':
-          app.run(host='0.0.0.0', port=5000)
-          EOF2
-          nohup python3 /home/ubuntu/app.py &
-        EOF
+        #!/bin/bash
+
+echo "Update/Install required OS packages"
+yum update -y
+yum install -y httpd wget php-fpm php-mysqli php-json php php-devel telnet tree git
+
+echo "Deploy PHP info app"
+cd /tmp
+git clone https://github.com/kledsonhugo/app-dynamicsite
+cp /tmp/app-dynamicsite/phpinfo.php /var/www/html/index.php
+
+echo "Config Apache WebServer"
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+
+echo "Start Apache WebServer"
+systemctl enable httpd
+service httpd restart
+        EOF 
     }
     os_profile_linux_config {
         disable_password_authentication = false
     }
 }
 
-#vm03 publica
+#vm3 publica
 
 resource "azurerm_public_ip" "vm03_pip_public" {
     name                = "vm03-pip-public"
     location            = var.rglocation
   resource_group_name = var.rgname
     allocation_method   = "Static"
-    domain_name_label   = "vm03-pip-public1a"
+    domain_name_label   = "vm03-pip-public"
 }
 
-resource "azurerm_network_interface" "vm03_nic_public1a" {
+resource "azurerm_network_interface" "vm03_nic_public" {
     name                = "vm03-nic-public"
     location            = var.rglocation
   resource_group_name = var.rgname
     ip_configuration {
-        name                          = "vm03-ipconfig-public1a"
-        subnet_id                     = var.snvnetapub1b
+        name                          = "vm03-ipconfig-public"
+        subnet_id                     = var.snvnetapub1a
         private_ip_address_allocation = "Dynamic"
         public_ip_address_id          = azurerm_public_ip.vm03_pip_public.id
     }
 }
 
 resource "azurerm_virtual_machine" "vm03_public" {
-    name                          = "vm03-public"
+    name                = "vm03-public"
     location            = var.rglocation
   resource_group_name = var.rgname
-    network_interface_ids         = [azurerm_network_interface.vm03_nic_public1a.id]
-    vm_size                       = "Standard_E2s_v3"
+    network_interface_ids         = [azurerm_network_interface.vm03_nic_public.id]
+    vm_size                       = "Standard_D2s_v3"
     delete_os_disk_on_termination = true
     storage_image_reference {
         publisher = "Canonical"
@@ -210,20 +224,27 @@ resource "azurerm_virtual_machine" "vm03_public" {
         admin_username = "azureuser"
         admin_password = "Password1234!" 
         custom_data    = <<-EOF
-         #!/bin/bash
-            sudo apt-get update
-            sudo apt-get install -y python3 python3-pip
-            pip3 install flask
-             cat << 'EOF2' > /home/ubuntu/app.py
-            from flask import Flask
-             app = Flask(__name__)
-             @app.route('/')
-             def home():
-            return "Hello, Azure from Flask!"
-             if __name__ == '__main__':
-          app.run(host='0.0.0.0', port=5000)
-          EOF2
-          nohup python3 /home/ubuntu/app.py &
+        #!/bin/bash
+
+echo "Update/Install required OS packages"
+yum update -y
+yum install -y httpd wget php-fpm php-mysqli php-json php php-devel telnet tree git
+
+echo "Deploy PHP info app"
+cd /tmp
+git clone https://github.com/kledsonhugo/app-dynamicsite
+cp /tmp/app-dynamicsite/phpinfo.php /var/www/html/index.php
+
+echo "Config Apache WebServer"
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+
+echo "Start Apache WebServer"
+systemctl enable httpd
+service httpd restart
         EOF 
     }
     os_profile_linux_config {
@@ -231,34 +252,34 @@ resource "azurerm_virtual_machine" "vm03_public" {
     }
 }
 
-#vm04 publica
+#vm3 publica
 
 resource "azurerm_public_ip" "vm04_pip_public" {
     name                = "vm04-pip-public"
     location            = var.rglocation
   resource_group_name = var.rgname
     allocation_method   = "Static"
-    domain_name_label   = "vm03-pip-public1a"
+    domain_name_label   = "vm04-pip-public"
 }
 
-resource "azurerm_network_interface" "vm04_nic_public1a" {
+resource "azurerm_network_interface" "vm04_nic_public" {
     name                = "vm04-nic-public"
     location            = var.rglocation
   resource_group_name = var.rgname
     ip_configuration {
-        name                          = "vm04-ipconfig-public1a"
-        subnet_id                     = var.snvnetapub1b
+        name                          = "vm04-ipconfig-public"
+        subnet_id                     = var.snvnetapub1a
         private_ip_address_allocation = "Dynamic"
         public_ip_address_id          = azurerm_public_ip.vm04_pip_public.id
     }
 }
 
 resource "azurerm_virtual_machine" "vm04_public" {
-    name                          = "vm04-public"
+    name                = "vm04-public"
     location            = var.rglocation
   resource_group_name = var.rgname
-    network_interface_ids         = [azurerm_network_interface.vm04_nic_public1a.id]
-    vm_size                       = "Standard_E2s_v3"
+    network_interface_ids         = [azurerm_network_interface.vm04_nic_public.id]
+    vm_size                       = "Standard_D2s_v3"
     delete_os_disk_on_termination = true
     storage_image_reference {
         publisher = "Canonical"
@@ -276,20 +297,27 @@ resource "azurerm_virtual_machine" "vm04_public" {
         admin_username = "azureuser"
         admin_password = "Password1234!" 
         custom_data    = <<-EOF
-         #!/bin/bash
-            sudo apt-get update
-            sudo apt-get install -y python3 python3-pip
-            pip3 install flask
-             cat << 'EOF2' > /home/ubuntu/app.py
-            from flask import Flask
-             app = Flask(__name__)
-             @app.route('/')
-             def home():
-            return "Hello, Azure from Flask!"
-             if __name__ == '__main__':
-          app.run(host='0.0.0.0', port=5000)
-          EOF2
-          nohup python3 /home/ubuntu/app.py &
+        #!/bin/bash
+
+echo "Update/Install required OS packages"
+yum update -y
+yum install -y httpd wget php-fpm php-mysqli php-json php php-devel telnet tree git
+
+echo "Deploy PHP info app"
+cd /tmp
+git clone https://github.com/kledsonhugo/app-dynamicsite
+cp /tmp/app-dynamicsite/phpinfo.php /var/www/html/index.php
+
+echo "Config Apache WebServer"
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+
+echo "Start Apache WebServer"
+systemctl enable httpd
+service httpd restart
         EOF 
     }
     os_profile_linux_config {
@@ -300,26 +328,26 @@ resource "azurerm_virtual_machine" "vm04_public" {
 #lb association
 
 resource "azurerm_network_interface_backend_address_pool_association" "vm01" {
-    ip_configuration_name   = "vm01-pip-public"
-    network_interface_id    = azurerm_network_interface.vm01_nic_public1a.id
+    ip_configuration_name   = "vm01-ipconfig-public"
+    network_interface_id    = azurerm_network_interface.vm01_nic_public.id
     backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "vm02" {
-    ip_configuration_name   = "vm02-pip-public"
+    ip_configuration_name   = "vm02-ipconfig-public"
     network_interface_id    = azurerm_network_interface.vm02_nic_public.id
     backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "vm03" {
-    ip_configuration_name   = "vm03_pip_public"
-    network_interface_id    = azurerm_network_interface.vm03_nic_public1a.id
+    ip_configuration_name   = "vm03-ipconfig-public"
+    network_interface_id    = azurerm_network_interface.vm03_nic_public.id
     backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "vm04" {
-    ip_configuration_name   = "vm04_pip_public"
-    network_interface_id    = azurerm_network_interface.vm04_nic_public1a.id
+    ip_configuration_name   = "vm04-ipconfig-public"
+    network_interface_id    = azurerm_network_interface.vm04_nic_public.id
     backend_address_pool_id = azurerm_lb_backend_address_pool.lb.id
 }
 
